abdc6d251d981367d5c147ca757fcb1dc82be828
META-PSYCHE <metapsycheshop@gmail.com>
2026-03-01 22:35:20 -0800

    Update Stripe API version to 2026-02-25.clover

--- lib/services/stripe.service.ts
+++ lib/services/stripe.service.ts
@@ -11,7 +11,7 @@ function initializeStripe(): Stripe | null {
   }
 
   return new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-12-18.acacia',
+    apiVersion: '2026-02-25.clover',
     typescript: true,
   });
 }
